d1759a82a400ff280d7970c04448471d2a615d84
Inuun <dev.inuun@gmail.com>
2017-04-09 16:18:14 +0200

    use switch instead of if-else

--- commands/status.js
+++ commands/status.js
@@ -1,9 +1,10 @@
 const discord = require('discord.js');
+const https = require('https');
 const http = require('http');
 const config = require('../config.js');
 const lang = require('../lang.js');
 
-function pushMojangStatus(message) {
+function pushMojangStatus(message, callback) {
     let options = {
         host: 'api.kuvus.pl',
         port: 80,
@@ -23,6 +24,48 @@ function pushMojangStatus(message) {
                 .setURL(config.settings.website)
                 .addField(lang.pl_PL.commands.status.mojang_status + ': \n', reply)
             message.channel.sendEmbed(embed, {disableEveryone: true});
+            callback();
+        });
+    });
+}
+
+function pushServerStatus(message, ip, callback) {
+    let options = {
+        host: 'api.skript.pl',
+        port: 443,
+        path: '/server/' + encodeURIComponent(ip) + '/'
+    };
+
+    https.get(options).on('response', function (response) {
+        let reply = '';
+        response.on('data', function (chunk) {
+            reply += chunk;
+        });
+        response.on('end', function () {
+            let json = JSON.parse(reply);
+            if (json.online == true) {
+                var status = lang.pl_PL.commands.status.on;
+            } else {
+                var status = lang.pl_PL.commands.status.off;
+            }
+            if (json.online != false) {
+                const embed = new discord.RichEmbed()
+                    .setTitle(config.settings.bot_name)
+                    .setColor(config.settings.color.default)
+                    .setFooter(config.settings.footer)
+                    .setThumbnail(config.settings.bot_thumbnail)
+                    .setURL(config.settings.website)
+                    .addField('' + lang.pl_PL.commands.status.status + ':', json.address)
+                    .addField('➭ ' + lang.pl_PL.commands.status.status + ': ', status)
+                    .addField('➭ ' + lang.pl_PL.commands.status.ping + ': ', parseInt(json.latency) + ' ms')
+                    .addField('➭ ' + lang.pl_PL.commands.status.players + ': ', json.players.online + '/' + json.players.max)
+                    .addField('➭ ' + lang.pl_PL.commands.status.version + ': ', json.version.name)
+                    .addField('➭ ' + lang.pl_PL.commands.status.motd + ': ', json.description)
+                message.channel.sendEmbed(embed, {disableEveryone: true});
+            } else {
+                message.reply(lang.pl_PL.commands.status.server_offline);
+            }
+            callback();
         });
     });
 }
@@ -35,45 +78,16 @@ status.get = function (message) {
         let args = message.content.split(" ").slice(1);
         if (args[0] != undefined) {
             let arg = args[0].toLowerCase();
-            if (arg == 'mojang') {
-                pushMojangStatus(message);
-            } else {
-                let options = {
-                    host: 'api.kuvus.pl',
-                    port: 80,
-                    path: '/srv.php?s=' + encodeURIComponent(arg)
-                };
-
-                http.get(options).on('response', function (response) {
-                    let reply = '';
-                    response.on('data', function (chunk) {
-                        reply += chunk;
+            switch (arg) {
+                case 'mojang':
+                    pushMojangStatus(message, function () {
+                        return message.channel.stopTyping(true);
                     });
-                    response.on('end', function () {
-                        let json = JSON.parse(reply);
-                        if (json.online == true) {
-                            var status = lang.pl_PL.commands.status.on;
-                        } else {
-                            var status = lang.pl_PL.commands.status.off;
-                        }
-                        if (json.online != false) {
-                            const embed = new discord.RichEmbed()
-                                .setTitle(config.settings.bot_name)
-                                .setColor(config.settings.color.default)
-                                .setFooter((lang.pl_PL.commands.status.footer).replace('{0}', config.settings.footer))
-                                .setURL(config.settings.website)
-                                .addField('' + lang.pl_PL.commands.status.status + ':', json.address)
-                                .addField('➭ ' + lang.pl_PL.commands.status.status + ': ', status)
-                                .addField('➭ ' + lang.pl_PL.commands.status.ping + ': ', parseInt(json.latency) + ' ms')
-                                .addField('➭ ' + lang.pl_PL.commands.status.players + ': ', json.players.online + '/' + json.players.max)
-                                .addField('➭ ' + lang.pl_PL.commands.status.version + ': ', json.version.name)
-                                .addField('➭ ' + lang.pl_PL.commands.status.motd + ': ', json.description)
-                            message.channel.sendEmbed(embed, {disableEveryone: true});
-                        } else {
-                            message.reply(lang.pl_PL.commands.status.server_offline);
-                        }
+                    break;
+                default:
+                    pushServerStatus(message, arg, function () {
+                        return message.channel.stopTyping(true);
                     });
-                });
             }
         } else {
             message.channel.sendMessage(`<@${message.author.id}> ⚠️ **` + lang.pl_PL.valid_usage + `**: \`.status <mojang/ip>\``);
